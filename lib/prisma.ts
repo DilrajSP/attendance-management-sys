cac8f7ba71022a9bc0d39ea3ef34dae767ecd2df
Dilraj Singh <dil476raj@gmail.com>
2026-01-30 18:16:29 +0530

    testing

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,25 @@
 export const runtime = "nodejs";
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+import { PrismaClient } from "@prisma/client";
+import fs from "fs";
+import path from "path";
+
+const caPath = path.join(process.cwd(), "ca.pem");
+console.log("🔐 CA PEM exists:", fs.existsSync(caPath));
+
+// 👇 declare prisma on globalThis (TS-safe)
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
 const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
+    log: ["error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
